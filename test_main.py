45e595992edf746d9dabb5cff14dcc0af5bb1e41
BobZhang26 <141781876+BobZhang26@users.noreply.github.com>
2023-12-09 21:36:40 -0500

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -1,13 +1,13 @@
 """
 Test goes here
 """
-from main import add
+# from main import add
 
-def test_func():
-    return add(1,1)
+# def test_func():
+#     return add(1,1)
 
-if __name__ == "__main__":
-    assert test_func() == 2
+# if __name__ == "__main__":
+#     assert test_func() == 2
   
   
 
